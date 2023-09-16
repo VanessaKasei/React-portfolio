863687c5ecf22d587e8a261fec25d1bc512c7a5c
VanessaKasei <kaseivanessa@gmail.com>
2023-09-16 23:42:23 +0300

    added a footer

---
+++ src/pages/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css'
+function Footer() {
+    return(
+        <>
+           <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; Copyright 2023 KaseiVanessa</p>
+      </div>
+    </footer>
+        </>
+    );
+}
+export default Footer;
