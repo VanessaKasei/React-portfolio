74200421a5cf2b0a1c1c1181745503fe96fa6a3f
VanessaKasei <kaseivanessa@gmail.com>
2023-09-17 22:43:14 +0300

    ceated cards for my featured projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,11 +1,57 @@
 import React from "react";
+import './Projects.css'
 
 function Projects() {
     return(
         <section id="projects">
-
+            <h1 id="main-title">FEATURED PROJECTS </h1>
+            <div className="row1">
+                <div className="project-card1">
+                    <h2>Uprime Designs Landing Page</h2>
+                    <p>This is a simple landing page for a Design comapany
+                        that showcases details about the company such as 
+                        their contact and various social media platforms and generally
+                        the kind of designs that the are able to come up with.
+                        I created it using Reactjs for interactivity, CSS for styling and
+                        HTML
+                    </p>
+                </div>
+                <div className="project-card1">
+                    <h2>Uprime Designs Landing Page</h2>
+                    <p>This is a simple landing page for a Design comapany
+                        that showcases details about the company such as 
+                        their contact and various social media platforms and generally
+                        the kind of designs that the are able to come up with.
+                        I created it using Reactjs for interactivity, CSS for styling and
+                        HTML
+                    </p>
+                </div>
+            </div>
+            <div className="row2">
+                <div className="project-card1">
+                    <h2>Uprime Designs Landing Page</h2>
+                    <p>This is a simple landing page for a Design comapany
+                        that showcases details about the company such as 
+                        their contact and various social media platforms and generally
+                        the kind of designs that the are able to come up with.
+                        I created it using Reactjs for interactivity, CSS for styling and
+                        HTML
+                    </p>
+                </div>
+                <div className="project-card1">
+                    <h2>Uprime Designs Landing Page</h2>
+                    <p>This is a simple landing page for a Design comapany
+                        that showcases details about the company such as 
+                        their contact and various social media platforms and generally
+                        the kind of designs that the are able to come up with.
+                        I created it using Reactjs for interactivity, CSS for styling and
+                        HTML
+                    </p>
+                </div>
+            </div>
         </section>
     );
 }
 
 export default Projects;
+
