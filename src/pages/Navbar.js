e8708f6e144b01d2c0312b4186619f8e6ca45619
VanessaKasei <kaseivanessa@gmail.com>
2023-10-03 23:59:24 +0300

    styling navbar with tailwind css

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,15 +1,18 @@
-import React from 'react';
+// Navbar.js
+import React from "react";
 
 function Navbar() {
-    return (
-        <nav>
-            <ul>
-                <li><a href='#home'>Home</a></li>
-                <li><a href='#about'>About</a></li>
-                <li><a href='#projects'>Projects</a></li>
-                <li><a href='#contact'>Contact</a></li>
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="bg-gray-900 p-4 fixed top-0 w-full z-10">
+      <ul className="flex justify-end space-x-4">
+        <li><a href="#home" className="text-white">Home</a></li>
+        <li><a href="#about" className="text-white">About</a></li>
+        <li><a href="#skills" className="text-white">Skills</a></li>
+        <li><a href="#projects" className="text-white">Projects</a></li>
+        <li><a href="#contact" className="text-white">Contact</a></li>
+      </ul>
+    </nav>
+  );
 }
+
 export default Navbar;
