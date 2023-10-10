f0319d5f63e6396e766a25059993d6f0db5ee954
VanessaKasei <kaseivanessa@gmail.com>
2023-10-10 13:34:42 +0300

    about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,40 @@
 import React from "react";
+import PassportPhoto from "./Images/PassportPhoto.jpg";
 
 function About() {
-    return(
-        <section id="about">
-
-        </section>
-    );
+  return (
+    <section className="bg-gray-100 p-5">
+      <div className="container mx-auto">
+        <h1 className="font-extrabold font-sans text-center text-3xl mb-4">
+          About Me
+        </h1>
+        <div className="about-container max-w-full">
+          <img src={PassportPhoto} className="max-w-20% h-auto" alt="me" />
+          <p className="max-w-2xl mt-5 text-lg text-gray-700">
+            I am a highly skilled and competent web and mobile app developer with a 
+            focus in creating responsive, visually appealing and user-friendly applications
+            that meet the users requirements using technologies such as JavaScript, ReactJs,
+            TailwindCSS and Flutter. Let's connect and explore how we can innovate and create
+            teogether.
+          </p>
+        </div>
+        <div className="resume mt-5">
+          <a
+            href="https://drive.google.com/file/d/1ZjOGyQyyddmtooy4q4fNvNPEf-rMchFe/view?usp=sharing"
+            target="_blank" // Add this if you want the link to open in a new tab
+            rel="noopener noreferrer" // Recommended for security
+          >
+            <button
+              type="button"
+              className="rounded-lg bg-blue-800 text-white py-2 px-4 transition-transform duration-200 hover:scale-90"
+            >
+              Download Resume
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
