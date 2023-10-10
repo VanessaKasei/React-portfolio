82d0cdf41ddb30b4cc902af3443f58f13a1e3617
VanessaKasei <kaseivanessa@gmail.com>
2023-10-10 20:26:53 +0300

    adjusted vh

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,10 @@
 import React from "react";
 import './Home.css'
 import { Link } from "react-scroll";
-//import homepage3 from "./Images/homepage3.jpg";
 
 function Home() {
     return (
         <section id="home">
-            {/*<div className="image-container">
-                <img src={homepage3} id="image" alt="my pic" />
-            </div>*/}
             <div className="home-text">
                 <h3 id="h3">Hello, I'm</h3>
                 <h1 id="h4">Vanessa Kasei</h1>
