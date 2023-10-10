462e2b3bc41338f1d506ed47d3ee719bf460fbe6
VanessaKasei <kaseivanessa@gmail.com>
2023-10-10 20:27:44 +0300

    added buttons and styled them

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,20 +1,24 @@
 import React from "react";
 import './Projects.css'
 
-function Projects() {
+function Projects({title, deploymentLink}) {
     return(
         <section id="projects">
             <h1 id="main-title">FEATURED PROJECTS </h1>
             <div className="row1">
                 <div className="project-card1">
+                    <h3>{title}</h3>
                     <h2 className="p1">Uprime Designs Landing Page</h2>
                     <p>This is a simple landing page for a Design company
                         that showcases details about the company such as 
                         their contact and various social media platforms and generally
                         the kind of designs that the are able to come up with.
-                        I created it using Reactjs for interactivity, CSS for styling and
+                        I created it using ReactJs for interactivity, CSS for styling and
                         HTML
                     </p>
+                    <a href="https://vanessakasei.github.io/Upright/" target="_blank" rel="noopener noreferrer">
+                    <button className="view-button">View Project</button>  
+                    </a>
                 </div>
                 <div className="project-card1">
                     <h2 className="p2">Home listing website</h2>
@@ -22,6 +26,9 @@ function Projects() {
                         houses available plus some of the features that it contains. It 
                         was created using Reactjs and CSS.
                     </p>
+                    <a href="https://vanessakasei.github.io/React/" target="_blank" rel="noopener noreferrer">
+                    <button className="view-button">View Project</button>
+                    </a>
                 </div>
             </div>
             <div className="row2">
@@ -30,6 +37,9 @@ function Projects() {
                     <p>This is a website that showcases my work, accomplishments, skills
                         and expertise.
                     </p>
+                    <a href="https://vanessakasei.github.io/React/" target="_blank" rel="noopener noreferrer">
+                    <button className="view-button">View Project</button>
+                    </a>
                 </div>
                 <div className="project-card1">
                     <h2 className="p4">Precious Gifts</h2>
@@ -38,6 +48,9 @@ function Projects() {
                         to place their donations at various pick-up centers which will later on
                         be picked.
                     </p>
+                    <a href="https://vanessakasei.github.io/React/" target="_blank" rel="noopener noreferrer">
+                    <button className="view-button">Yet to be deployed</button>
+                    </a>
                 </div>
             </div>
         </section>
